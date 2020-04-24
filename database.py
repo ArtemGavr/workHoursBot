33ee226b5af07af5373af5c207b59e0919dcc833
ArtemGavr <58862869+ArtemGavr@users.noreply.github.com>
2020-04-24 16:03:16 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,9 @@
 import pymongo as pymongo
 
-client = pymongo.MongoClient("mongodb+srv://dbAdmin:dbAdminpass@cluster0-i7xdc.gcp.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodatabase_link")
 db = client.main
-
 
 Workers = db.Workers
 Locations = db.Locations
-Users = db.Users
 History = db.History
-CustomTables = db.CustomTables
-Metadata = db.Metadata
 TimeHandler = db.TimeHandler
